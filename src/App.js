7bd87c08f8a5f74a18490e5a3a87ca2761abd660
Surendhiran <132738912+surendhiran2000@users.noreply.github.com>
2024-08-07 10:12:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './loginform';
+import LoginForm from './loginform';
+function App() {
+  return (
+    <div className="App">
+     <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
